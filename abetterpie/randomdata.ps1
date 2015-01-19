fb2251dd318cbef3ca1f2b1da9ee6ae3fbd0a289
JeromeLefebvre <jerome.p.lefebvre@gmail.com>
2015-01-18 21:24:35 -0800

    Changed the node to -localhose

--- abetterpie/randomdata.ps1
+++ abetterpie/randomdata.ps1
@@ -1,11 +1,11 @@
 
 
 $tagname = "fastsinusoid2"
-.\abetterpie -Node Jerome-PI1 ".\examples\createTags.txt" $tagname
+.\abetterpie -Node localhost ".\examples\createTags.txt" $tagname
 
 $counter = 0
 while ($counter -le 1) {
 	$counter = $counter + 1
 	$val = [System.Math]::Sin($counter/10)
-	.\abetterpie -Node Jerome-PI1  ".\examples\adddata.txt" $tagname $val
+	.\abetterpie -Node localhost  ".\examples\adddata.txt" $tagname $val
 }
