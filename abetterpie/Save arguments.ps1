2632163f02b83d54584a33a2d76012fd38400b6e
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2015-02-12 13:14:01 +0900

    Add variables to PowerShell
    Added a script that allows the use of variables in Windows PowerShell
    ISE

---
+++ abetterpie/Save arguments.ps1
@@ -0,0 +1,23 @@
+﻿function Add-ISEScriptParameter
+{
+ # Enter parameter(s) you want to use as default
+ $paramText = Read-Host "Enter parameter(s) for $($psISE.CurrentFile.DisplayName)"
+ 
+ # Create hash if not exists
+ if (-not $Global:ISEScriptParamHash) { $Global:ISEScriptParamHash = @{} }
+ 
+ # Add new parameters to the hash
+ $Global:ISEScriptParamHash[$psISE.CurrentFile.FullPath] = $paramText
+}
+ 
+function Start-ISEScriptWithParameter
+{
+ # Find name of actual file name
+ $File = $psISE.CurrentFile.FullPath
+ 
+ # Run actual file with stored parameters
+ Invoke-Expression -Command "$File $($Global:ISEScriptParamHash[$File])"
+}
+ 
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add('Add param', {Add-ISEScriptParameter}, 'Ctrl+Shift+F5')
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add('Run with param', {Start-ISEScriptWithParameter}, 'Ctrl+F5')
--- abetterpie/Script1.ps1
+++
@@ -1,4 +0,0 @@
-
-. .\abetterpie
-
-hello
